7d71672a4df6ad834f55a31148a0473587f9df43
BryantGG77 <brayespro@gmail.com>
2025-01-09 17:25:40 -0500

    Realizando componentes header y footer py-6

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="footer flex justify-center items-center py-9 px-12 bg-[#0a1017] border-t-4 border-[#2271D1]">
+        <footer className="footer flex justify-center items-center py-6 px-12 bg-[#0a1017] border-t-4 border-[#2271D1]">
             <img src="./img/logo.png" alt="Logo AluraFlix" />
         </footer>
     )
--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import HeaderButton from "./HeaderButton/HeaderButton"
 const Header = () => {
     return (
-        <header className="header flex justify-between items-center py-9 px-12 bg-[#0a1017] border-b-4 border-[#2271D1]">
+        <header className="header flex justify-between items-center py-6 px-12 bg-[#0a1017] border-b-4 border-[#2271D1]">
             <img src="./img/logo.png" alt="Logo AluraFlix" />
             <nav className="flex gap-6">
                 <HeaderButton title="Home" link="/" />
